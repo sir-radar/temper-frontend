3a91b04a397e1c93b27152f5779b22f683a5b1fc
MAC <nwokikesamson@gmail.com>
2019-04-19 19:57:17 +0100

    add some helpful comment

--- src/store.js
+++ src/store.js
@@ -12,33 +12,41 @@ export default new Vuex.Store({
   mutations: {
     setPosts (state, posts) {
       let postLength = 5
+      //get first 5 posts
       state.posts.push(...posts.splice(0, postLength))
     },
     move (state, { index, direction, title }) {
       var old_index = index
       var new_index
+      //decide direction
       if (direction === 'up') {
         new_index = --index
       } else {
         new_index = ++index
       }
+      //error check
       if (new_index >= state.posts.length) {
         var k = new_index - state.posts.length + 1
         while (k--) {
           state.posts.push(undefined)
         }
       }
+      //moves post to new position
       state.posts.splice(new_index, 0, state.posts.splice(old_index, 1)[0])
+      // adds current move to history
       state.history.unshift({ 'post': title, 'old_index': old_index, 'new_index': new_index })
     },
     restore (state, { old_index, new_index, history_index, title }) {
+      //error check
       if (old_index >= state.posts.length) {
         var k = old_index - state.posts.length + 1
         while (k--) {
           state.posts.push(undefined)
         }
       }
+      //retores previous state of posts
       state.posts.splice(old_index, 0, state.posts.splice(new_index, 1)[0])
+      // remove move from history after restore
       state.history.splice(history_index, 1)
     }
   },
