4c9f71972953d6a8b0f867f08dac3147a254e16d
MAC <nwokikesamson@gmail.com>
2019-04-19 17:53:15 +0100

    fix eslint errors

--- src/store.js
+++ src/store.js
@@ -2,36 +2,36 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    posts:[],
-    history:[]
+    posts: [],
+    history: []
   },
   mutations: {
-    setPosts(state,posts){
+    setPosts (state, posts) {
       let postLength = 5
       state.posts.push(...posts.splice(0, postLength))
-
     },
-    move(state,{index,direction,title}){
+    move (state, { index, direction, title }) {
       var old_index = index
-      if(direction == 'up'){
-        var new_index = --index
-      }else{
-        var new_index = ++index
+      var new_index
+      if (direction === 'up') {
+        new_index = --index
+      } else {
+        new_index = ++index
       }
       if (new_index >= state.posts.length) {
-          var k = new_index - state.posts.length + 1
-          while (k--) {
-            state.posts.push(undefined)
-          }
+        var k = new_index - state.posts.length + 1
+        while (k--) {
+          state.posts.push(undefined)
+        }
       }
       state.posts.splice(new_index, 0, state.posts.splice(old_index, 1)[0])
-      state.history.push({'post':title, 'old_index':old_index, 'new_index':new_index})
+      state.history.push({ 'post': title, 'old_index': old_index, 'new_index': new_index })
     },
-    restore(state,{old_index, new_index, history_index}){
+    restore (state, { old_index, new_index, history_index }) {
       if (old_index >= state.posts.length) {
         var k = old_index - state.posts.length + 1
         while (k--) {
